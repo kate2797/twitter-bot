8bd7d84b614103b97455a4d1601e375e501fef54
kate2797 <katka.zaprazna@icloud.com>
2020-08-09 01:19:53 +0200

    Handling API keys

--- twitter.py
+++ twitter.py
@@ -1,3 +1,9 @@
 import tweepy
-import random
-from nltk.corpus import wordnet
+import time
+
+from secrets import API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
+
+auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
+api = tweepy.API(auth)
+
+api.update_status("Hello Tweepy")
