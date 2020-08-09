2f5984f5f59aabaf2dca54bb8fe1d059e846aa87
kate2797 <katka.zaprazna@icloud.com>
2020-08-09 18:12:31 +0200

    Adjusted the number of tweets liked

--- like_tweets_by_keywords.py
+++ like_tweets_by_keywords.py
@@ -10,16 +10,15 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 # Handle linking tweets based on multiple search terms
-number_of_tweets = 10
-terms_list = ["100DaysOfCode", "CodeNewbie",
-              "codenewbie", "100daysofcode"]
+number_of_tweets = 100
+terms_list = ["100DaysOfCode", "CodeNewbie"]
 
 for search_term in terms_list:
     for tweet in tweepy.Cursor(api.search, search_term).items(number_of_tweets):
         try:
             print("Liked!")
             tweet.favorite()
-            time.sleep(2)
+            time.sleep(6)
         except tweepy.TweepError as e:
             print(e.reason)
         except StopIteration:
